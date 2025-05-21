5c098cb78b6ba52b1f97bf987ed55e3c951a3cd0
sebaswit34 <sebastian.swit.2003@o2.pl>
2025-05-21 16:06:02 +0200

    Aktualizacja konfiguracji bazy danych

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const PORT = 3000;
-const DB_USER = "sebaswit46";
-const DB_PASS = "Pilkareczna17";
-const MONGODB_URI = "mongodb://localhost:27017/nazwaTwojejBazy";
+const DB_USER = "";
+const DB_PASS = "";
+const MONGODB_URI = `mongodb+srv://"":""@cluster0.wkiftrn.mongodb.net/shop?retryWrites=true&w=majority`
+
+
 
 
 module.exports = { PORT, DB_USER, DB_PASS, MONGODB_URI };
